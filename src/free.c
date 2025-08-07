69cb31eb977b786c80fb00eb01104d294fe6ab9c
4trastos <info@4trastos.com>
2025-08-07 11:17:56 +0200

    free under construction

--- src/free.c
+++ src/free.c
@@ -1,17 +1,60 @@
 #include "../incl/malloc.h"
 
+int    search_the_area(t_block *block)
+{
+    int success;
+
+    if (block->type == TINY)
+    {
+        success = munmap(tiny_head, sizeof(t_block));
+        if (success == -1)
+            return(-1);
+        return (0);
+    }
+    success = munmap(small_head, sizeof(t_block));
+    if (success == -1)
+        return (-1);
+    return (0);
+}
+
 void    free(void *ptr)
 {
     t_block *data_block;
+    t_block *next_block;
+    int     sucess;
 
     if (!ptr)
         return;
     
     // 1. Encontrar el bloque de memoria
     data_block = (t_block *)((char *)ptr - sizeof(t_block));
-    
-    // 2. Marcar el bloque como libre
     data_block->is_free = true;
     
     // 3. Fusión de bloques adyacentes
+    if (data_block->next != NULL && data_block->next->is_free == true)
+    {
+        next_block = data_block->next;
+        data_block->size += next_block->size + sizeof(t_block);
+        data_block->next = next_block->next;
+    }
+
+    if (data_block->next == NULL)
+    {
+        if (data_block->type == LARGE)
+        {
+            sucess = munmap(tiny_head, sizeof(t_block));
+            if (sucess == -1)
+            {
+                print_str("Error: No se puede liberar");
+                return (NULL);
+            }
+            return;
+        }
+        if (search_the_zone(data_block) == -1)
+        {   
+            print_str("Error: No se puede liberar");
+            return (NULL);
+        }        
+        return;
+    }
 }
